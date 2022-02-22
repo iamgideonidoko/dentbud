38b44dc81d0f55df3ef5b7b6b765b6f183332c1c
Gideon Idoko <iamgideonidoko@gmail.com>
2022-02-22 01:46:41 +0100

    Add helper files to verify access and refresh tokens

--- src/api/v1/helpers/jwt.helper.ts
+++ src/api/v1/helpers/jwt.helper.ts
@@ -1,4 +1,4 @@
-import jwt from 'jsonwebtoken';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 import constants from '../../../config/constants.config';
 
 /*  */
@@ -17,6 +17,24 @@ export const signRefreshToken = async (payload: string | object | Buffer): Promi
     jwt.sign(payload, constants.refreshTokenSecret, { expiresIn: constants.accessTokenSpan }, (err, token) => {
       if (err) reject(err);
       resolve(token as string);
+    });
+  });
+};
+
+export const verifyAccessToken = async (accessToken: string): Promise<string | JwtPayload | undefined> => {
+  return new Promise<string | JwtPayload | undefined>((resolve, reject) => {
+    jwt.verify(accessToken, constants.accessTokenSecret, (err, decoded) => {
+      if (err) return reject(err);
+      resolve(decoded);
+    });
+  });
+};
+
+export const verifyRefreshToken = async (refreshToken: string): Promise<string | JwtPayload | undefined> => {
+  return new Promise<string | JwtPayload | undefined>((resolve, reject) => {
+    jwt.verify(refreshToken, constants.refreshTokenSecret, (err, decoded) => {
+      if (err) return reject(err);
+      resolve(decoded);
     });
   });
 };
