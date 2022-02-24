89803efb5ae61861fb1d66674425b9d443d9bfcd
Gideon Idoko <iamgideonidoko@gmail.com>
2022-02-24 16:36:29 +0100

    Add custom DentBud channel connector

---
+++ addons/dentbud_channel.py
@@ -0,0 +1,79 @@
+import asyncio
+import inspect
+from sanic import Sanic, Blueprint, response
+from sanic.request import Request
+from sanic.response import HTTPResponse
+from typing import Text, Dict, Any, Optional, Callable, Awaitable, NoReturn
+
+import rasa.utils.endpoints
+from rasa.core.channels.channel import (
+    InputChannel,
+    CollectingOutputChannel,
+    UserMessage,
+)
+
+class DentBudInput(InputChannel):
+    """ DentBud input channel implementation """
+
+    # returns the name of this channel (dentbud);
+    def name(self) -> Text:
+        return "dentbud"
+
+    # blueprint for the sanic framework server
+    def blueprint(
+        self, on_new_message: Callable[[UserMessage], Awaitable[None]]
+    ) -> Blueprint:
+
+        dentbud_webhook = Blueprint(
+            "dentbud_webhook_{}".format(type(self).__name__),
+            inspect.getmodule(self).__name__,
+        )
+
+        @dentbud_webhook.route("/", methods=["GET"])
+        async def health(request: Request) -> HTTPResponse:
+            return response.json({
+                "name": "DentBud AI",
+                "description": "AI Webhook for DentBud, an AI-powered mobile assistant for students.",
+                "status": "ok"
+
+            })
+
+        @dentbud_webhook.route("/webhook", methods=["POST"])
+        async def receive(request: Request) -> HTTPResponse:
+            sender_id = request.json.get("sender") # method to get sender_id 
+            text = request.json.get("text") # method to fetch message from payload
+            input_channel = self.name() # method to fetch input channel
+            metadata = self.get_metadata(request) # method to get metadata
+
+            collector = CollectingOutputChannel()
+
+            print("sender_id => ", sender_id)
+            print("text => ", text)
+            print("input_channel =>", input_channel)
+            print("metadata => ", metadata)
+            print("collector => ", collector)
+
+            if not sender_id or not text:
+                return response.json({ 
+                    "status": "fail",
+                    "statusCode": 401,
+                    "message": "Please, enter all fields"
+                 },
+                 status=401);
+            
+            # include exception handling
+            
+            # send the user message to Rasa model and wait for the response to be sent back
+            await on_new_message(
+                UserMessage(
+                    text,
+                    collector,
+                    sender_id,
+                    input_channel=input_channel,
+                    metadata=metadata,
+                )
+            )
+
+            return response.json(collector.messages)
+
+        return dentbud_webhook
