6556bc66563889d5be895d63b294ce570329890d
Gideon Idoko <iamgideonidoko@gmail.com>
2022-10-30 18:58:28 +0100

    feat: add accordion section for courses

--- frontend-rn/src/interfaces/helper.interface.ts
+++ frontend-rn/src/interfaces/helper.interface.ts
@@ -3,3 +3,16 @@ import type { DrawerScreenProps as ScreenProps } from '@react-navigation/drawer'
 type ParamListBase = Record<string, object | undefined>;
 
 export type DrawerScreenProps = ScreenProps<ParamListBase>;
+
+export interface AccordionSection<T = string> {
+  title: string;
+  content: T;
+}
+
+export type AccordionRenderFC<AdditionalArgs = Record<string, unknown>, ContentType = string> = (
+  args: AdditionalArgs,
+  section: AccordionSection<ContentType>,
+  index: number,
+  isActive: boolean,
+  sections?: AccordionSection<ContentType>[],
+) => JSX.Element;
--- frontend-rn/src/screens/Course.tsx
+++ frontend-rn/src/screens/Course.tsx
@@ -1,19 +1,97 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import CustomHeader from '../components/CustomHeader';
-import type { DrawerScreenProps } from '../interfaces/helper.interface';
-import { useIsConnected } from 'react-native-offline';
+import type { DrawerScreenProps, AccordionRenderFC, AccordionSection } from '../interfaces/helper.interface';
+import Accordion from 'react-native-collapsible/Accordion';
 
-const Course: React.FC<DrawerScreenProps> = ({ navigation }) => {
-  const isConnected = useIsConnected();
+const courseSectionTitle: AccordionRenderFC = (_, __, index) => {
+  if (index !== 0) return <></>;
   return (
-    <View>
-      <CustomHeader navigation={navigation} />
-      <Text>Hi from Course Page</Text>
-      {isConnected ? <Text>CONNECTED</Text> : <Text>NOT CONNECTED</Text>}
-      {/* <MenuIcon width={40} height={40} /> */}
+    <View style={styles.sectionTitle}>
+      <Text>Below are the courses you're currently offering:</Text>
     </View>
   );
 };
+
+const courseHeader: AccordionRenderFC = (_, section, __, isActive) => {
+  return (
+    <View style={[styles.header, { backgroundColor: isActive ? '#4845d2' : '#dadaf6' }]}>
+      <Text style={[styles.headerText, { color: isActive ? 'white' : '#070715' }]}>{section.title}</Text>
+    </View>
+  );
+};
+
+const courseContent: AccordionRenderFC = (_, section) => {
+  return (
+    <View style={styles.content}>
+      <Text>{section.content}</Text>
+    </View>
+  );
+};
+
+const Course: React.FC<DrawerScreenProps> = ({ navigation }) => {
+  const [activeSections, setActiveSections] = useState<number[]>([]);
+  const [sections] = useState<AccordionSection[]>([
+    {
+      title: 'First',
+      content: 'First Lorem ipsum...',
+    },
+    {
+      title: 'Second',
+      content: 'Second Lorem ipsum...',
+    },
+    {
+      title: 'Third',
+      content: 'Third Lorem ipsum...',
+    },
+  ]);
+
+  const handleAccordionChange = (newActiveSections: any) => {
+    console.log('active sections => ', newActiveSections);
+    setActiveSections(newActiveSections);
+  };
+  return (
+    <View>
+      <CustomHeader navigation={navigation} title="Course" />
+      <ScrollView style={styles.scrollView}>
+        <Accordion
+          sections={sections}
+          activeSections={activeSections}
+          renderSectionTitle={(...args) => courseSectionTitle({}, ...args)}
+          renderHeader={(...args) => courseHeader({}, ...args)}
+          renderContent={(...args) => courseContent({}, ...args)}
+          onChange={handleAccordionChange}
+        />
+      </ScrollView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: '#dadaf6',
+    padding: 10,
+    margin: 0,
+  },
+  headerText: {
+    textAlign: 'left',
+    fontSize: 16,
+    fontWeight: '500',
+    margin: 0,
+  },
+  content: {
+    padding: 20,
+    margin: 0,
+    backgroundColor: '#edecfb',
+  },
+  scrollView: {
+    marginBottom: 60,
+  },
+  sectionTitle: {
+    padding: 10,
+    paddingVertical: 20,
+  },
+});
 
 export default Course;
