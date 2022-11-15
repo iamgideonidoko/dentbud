f786f9a0a39190a9acde204ba7b42472fd9ddd59
Gideon Idoko <iamgideonidoko@gmail.com>
2022-11-15 04:20:42 +0100

    chore: add setting coming soon

--- frontend-rn/src/screens/Setting.tsx
+++ frontend-rn/src/screens/Setting.tsx
@@ -9,7 +9,8 @@ const Setting: React.FC<DrawerScreenProps> = ({ navigation }) => {
     <View style={{ flex: 1 }}>
       <CustomHeader navigation={navigation} title="Setting" />
       <ScrollView style={{ padding: 10 }}>
-        <Text style={[globalStyles.text, { fontSize: 16 }]}>Configure the settings in this app.</Text>
+        <Text style={[globalStyles.text, { fontStyle: 'italic', marginTop: 20 }]}>Setting coming soon...</Text>
+        <Text style={[globalStyles.text, { marginTop: 20 }]}>App built with ❤ by Gideon Idoko.</Text>
       </ScrollView>
       {/* <MenuIcon width={40} height={40} /> */}
     </View>
